140d35553083bf9788953a9e01504863bf6862bc
fntp <stickpoint@163.com>
2024-11-23 16:24:10 +0800

    help project 001

--- src/main/java/org/github/stickpoint/fxdemo/typewriterdemoforstudy/HelloController.java
+++ src/main/java/org/github/stickpoint/fxdemo/typewriterdemoforstudy/HelloController.java
@@ -2,7 +2,13 @@ package org.github.stickpoint.fxdemo.typewriterdemoforstudy;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
+/**
+ * typewriter-demo-for-study
+ * @author Fntp
+ * @e-mail fntp66@gmail.com
+ * @date 2024/11/23
+ * Help_address: https://www.reddit.com/r/JavaFX/comments/1gx9jqs/creating_delay_with_javafx/
+ */
 public class HelloController {
     @FXML
     private Label welcomeText;
